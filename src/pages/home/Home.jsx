50220189857f7b29281e4818a85cc3fc4d978c0b
mirry18299 <141658211+mirry18299@users.noreply.github.com>
2023-11-24 11:05:19 +0300

    error fixing

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./home.css"
 import Navbar from "../../components/navbar/Navbar"
 import Header from "../../components/header/Header"
-import Featured from "../../components/Featured/Featured";
+import Featured from "../../components/featured/Featured";
 import PropertyList from "../../components/propertyList/PropertyList";
-import FeaturedProperties from "../../components/FeaturedProperties/FeaturedProperties";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import MailList from "../../components/mailList/MailList";
 import Footer from "../../components/footer/Footer";
 
